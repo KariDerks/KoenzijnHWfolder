d89f8784be7f3b1ed8fee30abe1255dc53a22901
Basil-iskos <louise.vanbeek@live.nl>
2024-10-16 11:55:56 +0200

    imst1

---
+++ IMST/Assets/Scripts2/Interact_test_script.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact_test_script : MonoBehaviour
+{
+
+    public void test() {
+        Debug.Log("check");
+    }
+
+}
