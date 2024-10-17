71910a8a3a65c734975b09d446b59dad20521069
Basil-iskos <louise.vanbeek@live.nl>
2024-10-17 12:00:58 +0200

    IMST8
    travel werkt

--- IMST/Assets/scripts/Travel.cs
+++ IMST/Assets/scripts/Travel.cs
@@ -8,11 +8,13 @@ public class Travel : MonoBehaviour
 {
     public GameObject travelUI;
     public GameObject koen;
+    public GameObject crosshair;
 
     public void travelinteract() {
         travelUI.SetActive(true);
         Cursor.visible = true;
         koen.SetActive(false);
+        crosshair.SetActive(false);
         Cursor.lockState = CursorLockMode.None;
     }
 
@@ -20,12 +22,36 @@ public class Travel : MonoBehaviour
         travelUI.SetActive(false);
         koen.SetActive(true);
         Cursor.visible = false;
+        crosshair.SetActive(true);
         Cursor.lockState = CursorLockMode.Locked;
 
     }
 
     public void travelwoman() {
         SceneManager.LoadScene("oldlady", LoadSceneMode.Single);
+         travelUI.SetActive(false);
+        koen.SetActive(true);
+        Cursor.visible = false;
+        crosshair.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void travelmain() {
+        SceneManager.LoadScene("main", LoadSceneMode.Single);
+         travelUI.SetActive(false);
+        koen.SetActive(true);
+        Cursor.visible = false;
+        crosshair.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void travelsupermarkt() {
+        SceneManager.LoadScene("supermarkt", LoadSceneMode.Single);
+         travelUI.SetActive(false);
+        koen.SetActive(true);
+        Cursor.visible = false;
+        crosshair.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
 }
